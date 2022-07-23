d5f0bbe87668f0dab5956d6718adcb325264a57e
izcoser <76838137+izcoser@users.noreply.github.com>
2022-07-22 23:54:26 -0300

    Update dfs_bfs.py
    Add depth to bfs.

--- misc/dfs_bfs.py
+++ misc/dfs_bfs.py
@@ -32,6 +32,22 @@ def bfs(graph, v, visited):
                 q.append(n)
     print()
 
+def bfs_with_depth(graph, v, visited):
+    depth = 0
+    q = []
+    q.append(v)
+    while len(q) > 0:
+        depth_size = len(q)
+        while depth_size > 0:
+            p = q.pop(0)
+            visited[p] = True
+            print(f'{p}, depth: {depth}')
+            for n in graph[p]:
+                if not visited[n]:
+                    q.append(n)
+            depth_size -= 1
+        depth += 1
+
 print('Running DFS')
 dfs(graph, 'a', visited)
 print()
@@ -40,3 +56,9 @@ visited = {i: False for i in 'abcdefgh'}
 
 print('Running BFS')
 bfs(graph, 'a', visited)
+
+
+visited = {i: False for i in 'abcdefgh'}
+
+print('Running BFS with depth.')
+bfs_with_depth(graph, 'a', visited)
