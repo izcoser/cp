52682753d68dd15df113813db670f7066b113f33
Igor Coser <76838137+izcoser@users.noreply.github.com>
2025-03-08 17:28:17 -0300

    Do 9. Palindrome Number
    Without converting to a string. LC says my solution is very slow though.

---
+++ leetcode/9. Palindrome Number.py
@@ -0,0 +1,29 @@
+from math import log10
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # negative numbers can't be palindrome.
+        if x < 0:
+            return False
+        
+        d = digits(x)
+        if d == 1:
+            return True
+        
+        for i in range((d // 2) + 1):
+            rightmost = get_nth_digit(x, i)
+            leftmost = get_nth_digit(x, d - i - 1)
+
+            if rightmost != leftmost:
+                return False
+            
+        return True
+        
+def get_nth_digit(x: int, n: int) -> int:
+    return (x // (int)(10 ** (n))) % 10
+
+def digits(x: int) -> int:
+    if x == 0:
+        return 1
+    return (int)(log10(x)) + 1
+
