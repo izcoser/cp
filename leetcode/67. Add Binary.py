2c57a5e2d56ee19ab614f69f9e4984778e2c888b
Igor Coser <76838137+izcoser@users.noreply.github.com>
2025-03-16 16:05:22 -0300

    Add simpler and cleaner version

--- leetcode/67. Add Binary.py
+++ leetcode/67. Add Binary.py
@@ -34,3 +34,21 @@ class Solution:
         if carry:
             return '1' + ''.join(str(i) for i in output)
         return ''.join(str(i) for i in output)
+
+    def addBinaryImproved(self, a: str, b: str) -> str:
+        max_len = max(len(a), len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
+        
+        output = []
+        carry = 0
+
+        for i in range(max_len - 1, -1, -1):
+            total = carry + int(a[i]) + int(b[i])
+            output.append(str(total % 2))
+            carry = total // 2
+
+        if carry:
+            output.append('1')
+
+        return ''.join(output[::-1])
