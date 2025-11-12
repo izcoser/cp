fd159cd1fc5299499d7f9296e635d41e6a7e0735
Igor Coser <76838137+izcoser@users.noreply.github.com>
2025-11-11 23:12:37 -0300

    Do 100

---
+++ leetcode/100. Same Tree.py
@@ -0,0 +1,18 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(left, right):
+            if not left and not right:
+                return True
+            
+            if not left or not right:
+                return False
+
+            return left.val == right.val and dfs(left.left, right.left) and dfs(left.right, right.right)
+        
+        return dfs(p, q)
