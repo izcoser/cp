3368da0eb210362b7cec09edd3ec44fbd2a01c7f
Igor Coser <76838137+izcoser@users.noreply.github.com>
2025-11-16 15:28:55 -0300

    Do 61

---
+++ leetcode/61. Rotate List.py
@@ -0,0 +1,47 @@
+from typing import Optional, List
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if k == 0:
+            return head
+
+        l = list_len(head)
+        if l in (0, 1):
+            return head
+
+        rotations = k % l
+        print("list has len", l, "will perform ", rotations, "rotations")
+        if rotations == 0:
+            return head
+
+        slice_at_position = l - rotations
+        return list_slice_and_append_at_start(head, slice_at_position)
+
+def list_len(head: Optional[ListNode]) -> int:
+    i = 0
+    n = head
+    while n:
+        n = n.next
+        i += 1
+    return i
+
+def list_slice_and_append_at_start(head: Optional[ListNode], position: int) -> Optional[ListNode]:
+    # slice a list at `position`, append the sliced portion before the head.
+    i = 1
+    n = head
+    while i != position:
+        n = n.next
+        i += 1
+    
+    sliced = n.next
+    n.next = None
+
+    n = sliced
+    while n.next:
+        n = n.next
+    n.next = head
+    return sliced
