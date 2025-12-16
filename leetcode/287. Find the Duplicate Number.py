e87edda67456253babce10b7abd1520fbe2f397a
Igor Coser <76838137+izcoser@users.noreply.github.com>
2025-12-16 19:48:12 -0300

    Do Floyd in 287

--- leetcode/287. Find the Duplicate Number.py
+++ leetcode/287. Find the Duplicate Number.py
@@ -3,6 +3,8 @@ from typing import List
 # Couldn't figure a 2 pointers solution, but we are using a O(1) space and O(n) time as expected.
 # Q: How can we prove that at least one duplicate number must exist in nums?
 # A: The problem states `nums` has n + 1 integers in a [1, n] range, meaning there are more items than containers.
+
+# The Two Pointer solution uses Floyd's Cycle Detection.
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         appearances = [0] * (10 ** 5 + 1)
@@ -10,3 +12,18 @@ class Solution:
             appearances[i] += 1
             if appearances[i] > 1:
                 return i
+            
+    def findDuplicateFloyd(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
