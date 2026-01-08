d98e3a2e0107d15c37e19748273c08240b96e05c
Igor Coser <76838137+izcoser@users.noreply.github.com>
2026-01-08 10:16:31 -0300

    Do 383

---
+++ leetcode/383. Ransom Note.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+
+        for letter, count in ransom_count.items():
+            if letter not in magazine_count or magazine_count[letter] < count:
+                return False
+        return True
