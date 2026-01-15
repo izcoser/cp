3db0e953614bd167dc7cc3701e741fb4e73e6d5e
Igor Coser <76838137+izcoser@users.noreply.github.com>
2026-01-15 19:56:05 -0300

    Do 3042

---
+++ leetcode/3042. Count Prefix and Suffix Pairs I.py
@@ -0,0 +1,10 @@
+from typing import List
+# string matching, easy
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        count = 0
+        for i in range(len(words) - 1):
+            for j in range(i + 1, len(words)):
+                if words[j].startswith(words[i]) and words[j].endswith(words[i]):
+                    count += 1
+        return count
